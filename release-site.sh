938e94c31f6f3d2b8a9bdea73bfe7ca51bc3f643
Anthony Broome <anthonybroome87@gmail.com>
2017-06-23 11:47:41 +0100

    Scope after success to master only

--- release-site.sh
+++ release-site.sh
@@ -1,13 +1,11 @@
-cd $HOME
-git config --global user.email "travis@travis.com"
-git config --global user.name "Travis CI"
-
-git status
-git remote rm origin
-git remote add origin https://andrewharmellaw:$GITHUB_API_KEY@github.com/andrewharmellaw/quest-dist-sys-eng-java-apprentice.git
-git add -f .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
-git status
-git push -fq origin master > /dev/null
-git s
-echo -e "Done\n"
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  cd $HOME
+  git config --global user.email "travis@travis.com"
+  git config --global user.name "Travis CI"
+  touch .
+  git add -A .
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed [skip ci]"
+  git push origin master
+  echo -e "Done\n"
+fi
