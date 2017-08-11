ccbb9e627c48517f473cfd8358e0d6681f476067
Greg Wolverson <greg.wolverson@capgemini.com>
2017-08-11 08:03:36 +0100

    Moving non-skill dependencies into devDependencies section and cleaned up code in publish-to-local-docs file

--- publish-to-local-docs.js
+++ publish-to-local-docs.js
@@ -1,11 +1,6 @@
 var docPublisher = require('doc-publisher');
 var pkginfo = require('pkginfo')(module, 'dependencies');
 
-// Pull the dependency keys from the package.json file - filter only on skills,
-// ignoring any other dependencies
-const dependencies = Object.keys(module.exports.dependencies);
-let dependencyKeys = dependencies.filter((key) => {
-  return key.startsWith('skill');
-});
-
+// Pull the skill dependency keys from the package.json file 
+const dependencyKeys = Object.keys(module.exports.dependencies);
 docPublisher(dependencyKeys);
